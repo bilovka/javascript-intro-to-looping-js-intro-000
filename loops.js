a9dab9088a8d0eda65ac09e0029973bd6b227a45
Oleksandr <bilovka10@gmail.com>
2020-08-17 17:35:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop(array){
   for(var num=0;num<25;num++){
     if(num===1){
-      array="I am 1 strange loop.";
+      array[num]="I am 1 strange loop.";
     }
     else 
     array=`I am ${num} strange loops.`;
