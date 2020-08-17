eadf888408fcbebd58ee3739dcd27ff94583ec04
Oleksandr <bilovka10@gmail.com>
2020-08-17 17:32:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
   for(var num=0;num<25;num++){
     if(num===1){
-      array="I am 1 strange loop.";
+      array[num]="I am 1 strange loop.";
     }
     else 
-    array='I am ${i} strange loops.';
+    array[num]='I am ${i} strange loops.';
   }
   return array;
 }
